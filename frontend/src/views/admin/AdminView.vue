ee619e5b30f4f9b823d5dbcb25dcfff8dc59f212
sizoff <sizoff.dev@yandex.ru>
2024-10-12 10:24:43 +0300

    admin start

--- frontend/src/views/admin/AdminView.vue
+++ frontend/src/views/admin/AdminView.vue
@@ -1,15 +1,26 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import Button from "primevue/button";
 
 export default defineComponent({
-  name: "AdminView"
+  name: "AdminView",
+  components: {Button}
 })
 </script>
 
 <template>
-
+  <header>
+    <Button>Очередь</Button>
+    <Button>Ожидают проверки</Button>
+    <Button>Отклонено</Button>
+  </header>
+  <RouterView/>
 </template>
 
 <style scoped>
-
+header {
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+}
 </style>
--- frontend/src/views/admin/WaitingSubView.vue
+++ frontend/src/views/admin/WaitingSubView.vue
@@ -1,13 +1,17 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import Question from "@/components/Question.vue";
 
 export default defineComponent({
-  name: "WaitingSubView"
+  name: "WaitingSubView",
+  components: {Question}
 })
 </script>
 
 <template>
-
+  <main>
+    <Question v-for="(question, index) in questions" :question="question" :pending="pending" @edit="edit(index, $event)"/>
+  </main>
 </template>
 
 <style scoped>
